9ef7b7c4acbf8369252b3adc9b1ee84c5d3a5822
lambertn33 <lambertgithub@gmail.com>
2024-08-10 23:06:36 +0200

    feat: initial commit and installation

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h2 class="text-2xl font-semibold">Hello world</h2>
+    <n-button type="primary">naive-ui</n-button>
+  </div>
+</template>
+
+<style scoped></style>
