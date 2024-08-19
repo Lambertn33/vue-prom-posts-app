8a891e9aa8ed6bb7bf07bd6ddf45f5a4541c9a7d
lambertn33 <lambertgithub@gmail.com>
2024-08-19 19:30:38 +0200

    feat: filter posts

--- src/views/posts/PostsList.vue
+++ src/views/posts/PostsList.vue
@@ -10,9 +10,14 @@
             <div class="loader">Loading...</div>
         </div>
         <div v-else class="w-full">
-            <div class="flex justify-center py-4">
+            <div class="flex justify-center py-4 gap-2">
                 <input type="text" v-model="searchKey" placeholder="search post..."
                     class="border border-gray-400 px-4 py-2 rounded-full w-[600px]">
+                <select class="border border-gray-400 px-4 py-2 rounded-full w-[200px]" v-model="selectedFilter"
+                    v-if="isAuthenticated">
+                    <option value="all">All Posts</option>
+                    <option value="my-posts">My Posts</option>
+                </select>
             </div>
             <div v-if="isSearching" class="flex justify-center">
                 <p>Please wait....</p>
@@ -40,20 +45,29 @@ import PostListItem from '@/components/PostListItem.vue';
 
 const store = useStore();
 const searchKey = ref<string>('');
+const selectedFilter = ref<string>('all');
 const posts = computed<IPostList[]>(() => store.state.posts.posts.posts);
 const isLoading = computed<boolean>(() => store.state.posts.loading);
 const isSearching = computed<boolean>(() => store.state.posts.searching);
 const searchedPosts = computed<IPostList[]>(() => store.state.posts.searchedPosts.searchedPosts);
 const isAuthenticated = computed<boolean>(() => store.getters["auth/isAuthenticated"]);
+const authenticatedUser = computed(() => store.getters["auth/user"]);
 
 const postsToDisplay = computed(() => {
-    return searchKey.value !== "" && searchedPosts.value.length > 0
-        ? searchedPosts.value
-        : searchKey.value !== "" && searchedPosts.value.length === 0
-            ? []
-            : posts.value;
-});
+    let filteredPosts = posts.value;
 
+    if (selectedFilter.value === 'my-posts') {
+        filteredPosts = filteredPosts.filter(post => post.userId === authenticatedUser.value.id);
+    }
+
+    if (searchKey.value !== "" && searchedPosts.value.length > 0) {
+        return searchedPosts.value.filter(post => filteredPosts.includes(post));
+    } else if (searchKey.value !== "" && searchedPosts.value.length === 0) {
+        return [];
+    }
+
+    return filteredPosts;
+});
 
 const fetchPosts = () => {
     store.dispatch('posts/fetchPosts');
@@ -67,10 +81,6 @@ const debouncedSearch = debounce(() => {
 
 watch(searchKey, () => {
     debouncedSearch();
-});
-
-watch(() => store.state.posts.searchedPosts, (newSearchedPosts) => {
-    console.log('Searched Posts:', newSearchedPosts.searchedPosts);
 });
 
 onMounted(() => fetchPosts());
